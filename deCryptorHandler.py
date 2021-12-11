df8216101bf5f3cb4beb2cb9e298ca5894b6fc0b
Romanin <60302782+romanin-rf@users.noreply.github.com>
2021-12-11 14:08:26 +0200

    Update deCryptorHandler.py

--- deCryptorHandler.py
+++ deCryptorHandler.py
@@ -2,10 +2,13 @@ import os
 import sys
 
 class HandlerDict():
-    def __init__(self, decpyptor_path: str) -> None:
+    def __init__(self, decpyptor_path: str, *, python_path: str=None) -> None:
         self.decpyptor_path = os.path.abspath(decpyptor_path)
-        self.command_start = (("python " if (sys.platform == "win32") else "python3 ") if (os.path.splitext(self.decpyptor_path)[0] == ".py") else "") + self.decpyptor_path
-    
+        if python_path == None:
+            self.command_start = (("python " if (sys.platform == "win32") else "python3 ") if (os.path.splitext(self.decpyptor_path)[0] == ".py") else "") + self.decpyptor_path
+        else:
+            self.command_start = (python_path if (os.path.splitext(self.decpyptor_path)[0] == ".py") else "") + self.decpyptor_path
+
     def start(self, path: str, mode: str, *, key_path: str=None) -> dict:
         """Парсит dict информацию из deCryptor"""
         cmdl_argv = [mode, "--dict"]
@@ -13,7 +16,7 @@ class HandlerDict():
             cmdl_argv.append("--key \"" + key_path + "\"")
         cmdl_argv.append("\"" + path + "\"")
         return eval(os.popen("{0} {1}".format(self.command_start, " ".join(cmdl_argv))).read().split("\n")[1])
-    
+
     def get_info(self) -> dict:
         """Отдаёт dict с информацией о программе"""
         return eval(os.popen("{0} --version --dict".format(self.command_start)).read().split("\n")[1])
